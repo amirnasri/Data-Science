b2be627faba35f923aa881b3c155a4d808ab690a
amirnasri <amnasri@gmail.com>
2017-01-17 00:44:40 -0800

    -

--- Naive_Bayes/naive_bayes.py
+++ Naive_Bayes/naive_bayes.py
@@ -3,7 +3,9 @@ from sklearn.naive_bayes import BernoulliNB as BernoulliNB_SK
 from sklearn.naive_bayes import MultinomialNB as MultinomialNB_SK
 from sklearn.utils import check_random_state
 import time
-from utils import Utils
+import utils
+from scipy.sparse import issparse
+from utils import np_dot
 
 def timeit(method):
     def timed(*args, **kw):
@@ -21,7 +23,11 @@ class MultinomialNB(object):
         self.alpha = alpha
 
     def fit(self, X, y):
-        X, y = Utils.check_fit_input(X, y, binary=True)
+        #if issparse(X):
+        #    X = X.todense()
+        #if issparse(y):
+        #    y = y.todense()
+        #X, y = utils.check_fit_input(X, y, binary=True)
         m, n = X.shape
 
         Y = np.array([1 - y, y])
@@ -29,7 +35,7 @@ class MultinomialNB(object):
         # Calculate number of occurances for each feature for y={0, 1}
         # X_count is an array of shape (2, n) where aij is the number of
         # occurances for feature j given y=i.
-        X_count = Y.dot(X)
+        X_count = np_dot(Y, X)
 
         # Calculate total number of feature occurances for y={0, 1}
         # Y_count.shape = (2, )
@@ -38,7 +44,13 @@ class MultinomialNB(object):
         X_count_smoothed = X_count + self.alpha
         Y_count_smoothed = Y_count + self.alpha
 
-        Phi_X = np.diag(X_count_smoothed.sum(axis=1) ** -1).dot(X_count_smoothed)
+        Phi_X = np_dot(np.diag(X_count_smoothed.sum(axis=1) ** -1), X_count_smoothed)
+        print "X=%s" % X
+        print "X_count=%s" % X_count
+        print "Y=%s" % Y
+
+        print "Phi_X=%s" % Phi_X
+        print np.where(Phi_X <= 0)
         self.Phi_X_log = np.log(Phi_X)
         self.Phi_Y_log = np.log(Y_count_smoothed / Y_count_smoothed.sum())
 
@@ -47,7 +59,7 @@ class MultinomialNB(object):
 
     def predict_proba(self, X):
         # X.shape = (m, n), Phi_X_log.shape = (2, n)
-        log_prob = X.dot(self.Phi_X_log.T) + self.Phi_Y_log
+        log_prob = np_dot(X, self.Phi_X_log.T) + self.Phi_Y_log
 
         # log probabilities above are not normalized but are
         # enough to make a decision regarding class of y.
@@ -68,20 +80,20 @@ class BernoulliNB(object):
 
     @timeit
     def fit(self, X, y):
-        X, y = Utils.check_fit_input(X, y, binary=True)
+        X, y = utils.check_fit_input(X, y, binary=True)
         m, n = X.shape
 
         Y = np.array([1 - y, y])
         # Calculate number of occurances for each feature for y={0, 1}
         # X_count is an array of shape (2, n) where aij is the number of
         # occurances for feature j given y=i.
-        X_count = Y.dot(X)
+        X_count = np_dot(Y, X)
         Y_count = Y.sum(axis=1).astype(np.float)
 
         X_count_smoothed = X_count + self.alpha
         Y_count_smoothed = Y_count + 2 * self.alpha
 
-        Phi_X = np.diag(Y_count_smoothed**-1).dot(X_count_smoothed)
+        Phi_X = np_dot(np.diag(Y_count_smoothed**-1), X_count_smoothed)
 
         self.Phi_X_y0_log = np.log(np.c_[1 - Phi_X[0, :], Phi_X[0, :]])
         self.Phi_X_y1_log = np.log(np.c_[1 - Phi_X[1, :], Phi_X[1, :]])
@@ -95,8 +107,8 @@ class BernoulliNB(object):
         m = X.shape[0]
         metric = np.zeros((m, 2))
         # p(X\y = 0) p(y=0) , p(X\y = 1) p(y=1)
-        metric[:, 0] = (1 - X).dot(self.Phi_X_y0_log[:, 0]) + X.dot(self.Phi_X_y0_log[:, 1]) + self.Phi_Y_log[0]
-        metric[:, 1] = (1 - X).dot(self.Phi_X_y1_log[:, 0]) + X.dot(self.Phi_X_y1_log[:, 1]) + self.Phi_Y_log[1]
+        metric[:, 0] = np_dot(1 - X, self.Phi_X_y0_log[:, 0]) + np_dot(X, self.Phi_X_y0_log[:, 1]) + self.Phi_Y_log[0]
+        metric[:, 1] = np_dot(1 - X, self.Phi_X_y1_log[:, 0]) + np_dot(X, self.Phi_X_y1_log[:, 1]) + self.Phi_Y_log[1]
         return metric
 
 
