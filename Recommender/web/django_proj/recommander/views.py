4542aee894062d3e79ed78352e9b393c575b93a5
amirnasri <amnasri@gmail.com>
2017-06-21 01:31:16 -0700

    -

--- Recommender/web/django_proj/recommander/views.py
+++ Recommender/web/django_proj/recommander/views.py
@@ -2,7 +2,41 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import requests
+from bs4 import BeautifulSoup
 
+def load_pickle(name):
+    with open(name, 'rb') as f:
+        return pickle.load(f)
+
+url_cache = {}
+cwd = os.getcwd()
+pp_sim = np.load('data/pp_sim.npy')
+movie_index_to_ID = load_pickle('data/movie_index_to_ID.pkl')
+movies_df = pd.read_pickle('data/movies_df.pkl')
+recom_movie_index = np.argsort(pp_sim[0, :])[::-1][:10]
+recom_movie_id_df = pd.DataFrame({'MovieID': [movie_index_to_ID[i] for i in recom_movie_index]})
+recom_movie_df = pd.merge(recom_movie_id_df, movies_df, how='inner', on='MovieID')
+
+def get_poster_imdb(url):
+    if url in url_cache:
+        return url_cache[url]
+    try:
+        resp = requests.get(url)
+        bs = BeautifulSoup(resp.content, "lxml")
+        base_url = 'http://www.imdb.com/'
+        url_poster = base_url + bs.find("div", class_="poster").a['href']
+        resp = requests.get(url_poster)
+        bs = BeautifulSoup(resp.content, 'lxml')
+        src_url = bs.find('meta', itemprop='image')['content']
+        url_cache[url] = src_url
+        return src_url
+    except:
+        return ""
 
 
 def index(request):
@@ -13,7 +47,20 @@ def main(request):
     qs = request.environ['QUERY_STRING']
     names = [i.split('=')[1] for i in qs.split('&')]
     #return HttpResponse("Hello %s " % " ".join(names))
-    context = {'request': request}
-    context = {'a':int(request.GET['r1'])}
+    #context = {'a':int(request.GET['r1'])}
+    context = {}
+    url_list = []
+    recom_movie_index = np.argsort(pp_sim[0, :])[::-1][:10]
+    recom_movie_df = pd.merge(pd.DataFrame({'MovieID': [movie_index_to_ID[i] for i in recom_movie_index]}), movies_df, how='inner', on='MovieID')
+
+    img_urls = ''
+    for url in recom_movie_df['IMDb-URL'].tolist()[:1]:
+        img_urls += '<a href = "%s">' % url + \
+            '<img src="%s" style = "width:260px;height:380px;border:0">' % get_poster_imdb(url) + \
+            '</a>'
+
+    context['img_urls'] = img_urls
+    context['cwd'] = cwd
+    context['ls_output'] = os.listdir('data')
     return render(request, 'recommander/main.html', context)
 
