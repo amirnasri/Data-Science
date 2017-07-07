0a792418cf546f3cdb1f55f8fc9dc3823a5b6323
Amir <amir@fortinet>
2017-07-06 17:01:13 -0700

    -

--- Recommender/web/website/website/urls.py
+++ Recommender/web/website/website/urls.py
@@ -1,4 +1,4 @@
-"""django_proj URL Configuration
+"""website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
