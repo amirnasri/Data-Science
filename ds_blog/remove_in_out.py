3ac00e11a1b082673f4f0dd9e88d66b8f541e592
amirnasri <amnasri@gmail.com>
2018-07-11 19:45:29 -0700

    remove in-out from ipnb

---
+++ ds_blog/remove_in_out.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import re
+import os
+
+re_out = re.compile('Out\s*\[[0-9]+\]:')
+re_in1 = re.compile('In \[[0-9]+\]:')
+re_in2 = re.compile('In\xc2\xa0\[[0-9]+\]:')
+
+
+def remove_in_out(fname):
+    f = open(fname)
+    s = f.read()
+    s = re_in1.sub('', s)
+    s = re_in2.sub('', s)
+    s = re_out.sub('', s)
+
+    fout = open(fname, 'w')
+    fout.write(s)
+    fout.close()
+
+
+for fname in os.listdir('.'):
+    if fname.endswith('.html'):
+        remove_in_out(fname)
+
+
